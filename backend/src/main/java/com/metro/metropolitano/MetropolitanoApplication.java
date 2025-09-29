ab38cb0e194cc2c6d78bd89a7bcdb0e5eec97fd0
nguyen van cuong <cuongnvse183645@fpt.edu.vn>
2025-09-29 10:46:56 +0700

    Initial commit: add backend and frontend source code

---
+++ backend/src/main/java/com/metro/metropolitano/MetropolitanoApplication.java
@@ -0,0 +1,13 @@
+package com.metro.metropolitano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetropolitanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetropolitanoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/metro/metropolitano/MetropolitanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metro.metropolitano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetropolitanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
